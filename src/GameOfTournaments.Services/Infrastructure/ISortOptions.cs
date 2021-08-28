c32bf2931dec08fd240eadc1a01e9740310639ef
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 15:46:43 +0300

    Introduced sorting options

---
+++ src/GameOfTournaments.Services/Infrastructure/ISortOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Represents an interface, specifying sort options for the given <typeparamref name="TEntity"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity that will be used when sorting.</typeparam>
+    /// <typeparam name="TSortKey">The type of the property that will be used to sort on.</typeparam>
+    public interface ISortOptions<TEntity, TSortKey>
+    {
+        /// <summary>
+        /// Gets a property that indicates if the sort is in an ascending order.
+        /// </summary>
+        bool Ascending { get; }
+    
+        /// <summary>
+        /// Gets the sort expression that is used when sorting entities of type <typeparamref name="TEntity"/>.
+        /// </summary>
+        Expression<Func<TEntity, TSortKey>> SortExpression { get; }
+    }
+}
---
+++ src/GameOfTournaments.Services/Infrastructure/SortOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public class SortOptions<TEntity, TSortKey> : ISortOptions<TEntity, TSortKey>
+    {
+        public SortOptions(bool @ascending, Expression<Func<TEntity, TSortKey>> sortExpression)
+        {
+            this.Ascending = @ascending;
+            this.SortExpression = sortExpression ?? throw new ArgumentNullException(nameof(sortExpression));
+        }
+
+        /// <inheritdoc />
+        public bool Ascending { get; }
+
+        /// <inheritdoc />
+        public Expression<Func<TEntity, TSortKey>> SortExpression { get; }
+    }
+}
