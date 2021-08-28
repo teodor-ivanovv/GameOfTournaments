302bc36163af7db45663150d5de09234c404ad76
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 15:47:09 +0300

    Introduced all needed variances of get options

---
+++ src/GameOfTournaments.Services/Infrastructure/GetOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public class GetOptions<TEntity> : IGetOptions<TEntity>
+    {
+        /// <inheritdoc />
+        public Expression<Func<TEntity, bool>> FilterExpression { get; set; }
+    }
+  
+    public class GetOptions<TEntity, TSortKey> : GetOptions<TEntity>, IGetOptions<TEntity, TSortKey>
+    {
+        /// <inheritdoc />
+        public ISortOptions<TEntity, TSortKey> Sort { get; set; }
+
+        /// <inheritdoc />
+        public IPageOptions Pagination { get; set; }
+    }
+  
+    public class GetOptions<TEntity, TSortKey, TResult> : GetOptions<TEntity, TSortKey>, IGetOptions<TEntity, TSortKey, TResult>
+    {
+        /// <inheritdoc />
+        public IProjectionOptions<TEntity, TResult> Projection { get; set; }
+    }
+}
---
+++ src/GameOfTournaments.Services/Infrastructure/IGetOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public interface IGetOptions<TEntity>
+    {
+        Expression<Func<TEntity, bool>> FilterExpression { get; set; }
+    }
+
+    public interface IGetOptions<TEntity, TSortKey> : IGetOptions<TEntity>
+    {
+        IPageOptions Pagination { get; set; }
+
+        ISortOptions<TEntity, TSortKey> Sort { get; set; }
+    }
+
+    public interface IGetOptions<TEntity, TSortKey, TResult> : IGetOptions<TEntity, TSortKey>
+    {
+        IProjectionOptions<TEntity, TResult> Projection { get; set; }
+    }
+}
