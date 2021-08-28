45a502efe9ab49d044dd0a81f870bd7eb9dfcf88
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 15:46:53 +0300

    Introduced projection options

---
+++ src/GameOfTournaments.Services/Infrastructure/IProjectionOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// Represents an interface, specifying projection options for the given <typeparamref name="TSource"/>.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source entity that will be used to transform/map to <typeparamref name="TResult"/>.</typeparam>
+    /// <typeparam name="TResult">The type of the result after the projection has been applied.</typeparam>
+    public interface IProjectionOptions<TSource, TResult>
+    {
+        /// <summary>
+        /// Gets the expression projection selector used to project properties for the given <typeparamref name="TSource"/>.
+        /// </summary>
+        Expression<Func<TSource, TResult>> Selector { get; }
+    }
+}
---
+++ src/GameOfTournaments.Services/Infrastructure/ProjectionOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public class ProjectionOptions<TSource, TResult> : IProjectionOptions<TSource, TResult>
+    {
+        public ProjectionOptions(Expression<Func<TSource, TResult>> selector)
+        {
+            this.Selector = selector ?? throw new ArgumentNullException(nameof(selector));
+        }
+
+        /// <inheritdoc />
+        public Expression<Func<TSource, TResult>> Selector { get; }
+    }
+}
