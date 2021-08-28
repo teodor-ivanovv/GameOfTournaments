c375be116f79890e9a2cdbe42b37d09616d0858d
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 14:01:53 +0300

    Application user more properties

--- src/GameOfTournaments.Data/Models/ApplicationUser.cs
+++ src/GameOfTournaments.Data/Models/ApplicationUser.cs
@@ -4,5 +4,14 @@
 
     public class ApplicationUser : IdentityUser<int>
     {
+        public int Age { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool Male { get; set; }
     }
 }
