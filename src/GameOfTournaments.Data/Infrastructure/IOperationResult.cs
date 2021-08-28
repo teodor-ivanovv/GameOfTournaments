c7739e36f93212c7d505e8ca5ff29813a138e816
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 14:01:39 +0300

    Introduced operation result

---
+++ src/GameOfTournaments.Data/Infrastructure/IOperationResult.cs
@@ -0,0 +1,27 @@
+﻿namespace GameOfTournaments.Data.Infrastructure
+{
+    using System.Collections.Generic;
+
+    public interface IOperationResult : IOperationResult<object>
+    {
+    }
+
+    public interface IOperationResult<out T>
+    {
+        bool Success { get; }
+        
+        int Code { get; }
+        
+        T Object { get; }
+        
+        IEnumerable<string> Errors { get; }
+
+        IOperationResult<T> AddError(string errorMessage);
+
+        IOperationResult<T> ValidateNotNull(object obj, string className, string method, string parameter);
+        
+        IOperationResult<T> ValidateNotWhiteSpace(string obj, string className, string method, string parameter);
+
+        IOperationResult<T> SetCode(int code);
+    }
+}
---
+++ src/GameOfTournaments.Data/Infrastructure/OperationResult.cs
@@ -0,0 +1,62 @@
+﻿namespace GameOfTournaments.Data.Infrastructure
+{
+    using System.Collections.Generic;
+    using Ardalis.GuardClauses;
+
+    public class OperationResult : OperationResult<object>
+    {
+    }
+
+    public class OperationResult<T> : IOperationResult<T>
+    {
+        private readonly List<string> errors = new();
+        
+        public bool Success { get; private set; } = true;
+
+        public int Code { get; private set; }
+
+        public T Object { get; private set; }
+
+        public IEnumerable<string> Errors => this.errors;
+
+        public IOperationResult<T> AddError(string errorMessage)
+        {
+            Guard.Against.NullOrWhiteSpace(errorMessage, nameof(errorMessage));
+            this.Success = false;
+            this.errors.Add(errorMessage);
+
+            return this;
+        }
+
+        public IOperationResult<T> ValidateNotNull(object obj, string className, string method, string parameter)
+        {
+            Guard.Against.NullOrWhiteSpace(className, nameof(className));
+            Guard.Against.NullOrWhiteSpace(method, nameof(method));
+            Guard.Against.NullOrWhiteSpace(parameter, nameof(parameter));
+
+            if (obj == null)
+                this.AddError($"{nameof(this.ValidateNotNull)} - {className}.{method} - {parameter}.");
+
+            return this;
+        }
+
+        public IOperationResult<T> ValidateNotWhiteSpace(string obj, string className, string method, string parameter)
+        {
+            Guard.Against.NullOrWhiteSpace(className, nameof(className));
+            Guard.Against.NullOrWhiteSpace(method, nameof(method));
+            Guard.Against.NullOrWhiteSpace(parameter, nameof(parameter));
+
+            if (string.IsNullOrWhiteSpace(obj))
+                this.AddError($"{nameof(this.ValidateNotWhiteSpace)} - {className}.{method} - {parameter}.");
+
+            return this;
+        }
+
+        public IOperationResult<T> SetCode(int code)
+        {
+            this.Code = code;
+
+            return this;
+        }
+    }
+}
