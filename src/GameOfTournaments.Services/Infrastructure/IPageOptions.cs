4d440a6847e9cb77873012a6887512c71f97dc2e
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 15:46:35 +0300

    Introduced paging options

---
+++ src/GameOfTournaments.Services/Infrastructure/IPageOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    /// <summary>
+    /// Represents an interface, specifying pagination options.
+    /// </summary>
+    public interface IPageOptions
+    {
+        /// <summary>
+        /// Gets the page number.
+        /// </summary>
+        int Page { get; }
+
+        /// <summary>
+        /// Gets the count of entities per page.
+        /// </summary>
+        int Count { get; }
+    }
+}
---
+++ src/GameOfTournaments.Services/Infrastructure/PageOptions.cs
@@ -0,0 +1,25 @@
+﻿namespace GameOfTournaments.Services.Infrastructure
+{
+    using System;
+
+    public class PageOptions : IPageOptions
+    {
+        public PageOptions(int page, int count)
+        {
+            if (page <= 0)
+                throw new ArgumentOutOfRangeException(nameof(page), "Argument page must be greater than 0.");
+
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count), "Argument count must be greater than 0.");
+
+            this.Page = page;
+            this.Count = count;
+        }
+
+        /// <inheritdoc />
+        public int Page { get; }
+
+        /// <inheritdoc />
+        public int Count { get; }
+    }
+}
