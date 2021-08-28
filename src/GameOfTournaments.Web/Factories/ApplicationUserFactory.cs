435a9df75570eb53f56de40df9ee9842a2e5ba75
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-28 14:02:56 +0300

    Register user model and application user factory

---
+++ src/GameOfTournaments.Web/Factories/ApplicationUserFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace GameOfTournaments.Web.Factories
+{
+    using Ardalis.GuardClauses;
+    using GameOfTournaments.Data.Models;
+    using GameOfTournaments.Web.Models;
+
+    public static class ApplicationUserFactory
+    {
+        public static ApplicationUser Create(RegisterUserModel registerUserModel)
+        {
+            Guard.Against.Null(registerUserModel, nameof(registerUserModel));
+
+            return new ApplicationUser
+            {
+                UserName = registerUserModel.Username,
+                Email = registerUserModel.Email,
+                FirstName = registerUserModel.FirstName,
+                MiddleName = registerUserModel.MiddleName,
+                LastName = registerUserModel.LastName,
+                Age = registerUserModel.Age,
+                Male = registerUserModel.Male,
+                PhoneNumber = registerUserModel.PhoneNumber,
+            };
+        }
+    }
+}
---
+++ src/GameOfTournaments.Web/Models/RegisterUserModel.cs
@@ -0,0 +1,34 @@
+﻿namespace GameOfTournaments.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterUserModel
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string MiddleName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Range(18, 150)]
+        public int Age { get; set; }
+
+        public bool Male { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
