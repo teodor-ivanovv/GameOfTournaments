9af491c6548245bf0d09b8c4c38336ca7b281865
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-29 18:42:25 +0300

    Base controller sets authentication context for each request

--- src/GameOfTournaments.Web/Controllers/Controller.cs
+++ src/GameOfTournaments.Web/Controllers/Controller.cs
@@ -1,6 +1,11 @@
 ﻿namespace GameOfTournaments.Web.Controllers
 {
+    using System;
+    using System.Security.Claims;
+    using GameOfTournaments.Services;
+    using GameOfTournaments.Services.Infrastructure;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
     [Authorize]
@@ -8,5 +13,25 @@
     [Route("[controller]")]
     public abstract class Controller : ControllerBase
     {
+        protected IAuthenticationService AuthenticationService { get; }
+
+        protected Controller(IHttpContextAccessor httpContextAccessor, IAuthenticationService AuthenticationService)
+        {
+            var httpContext = httpContextAccessor?.HttpContext;
+            this.AuthenticationService = AuthenticationService ?? throw new ArgumentNullException(nameof(AuthenticationService));
+            var userId = httpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            var ipAddress = httpContext?.Connection?.RemoteIpAddress;
+            var parsed = int.TryParse(userId, out var integerId);
+            if (!parsed)
+                integerId = -1;
+            
+            this.AuthenticationService.Set(
+                new AuthenticationContext
+                {
+                    Id = integerId,
+                    ApplicationUser = httpContext?.User,
+                    IpAddress = ipAddress?.ToString(),
+                });
+        }
     }
 }
