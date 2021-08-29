2b2e16ad50be113e2d6379c266149c2949072791
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-08-29 18:41:21 +0300

    Implemented already seeded for role and user seeders. Improved the code execution to follow a better strategy and introduced logging.

--- src/GameOfTournaments.Seeding/RoleSeeder.cs
+++ src/GameOfTournaments.Seeding/RoleSeeder.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Threading.Tasks;
+    using GameOfTournaments.Data.Infrastructure;
     using GameOfTournaments.Data.Models;
+    using GameOfTournaments.Services;
     using GameOfTournaments.Shared;
     using Microsoft.AspNetCore.Identity;
     using static Shared.Actions; 
@@ -10,15 +12,22 @@
     public class RoleSeeder : IRoleSeeder
     {
         private readonly RoleManager<ApplicationRole> roleManager;
-
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly ILogger logger;
 
-        public RoleSeeder(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager)
+        public RoleSeeder(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager, ILogger logger)
         {
             this.roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
             this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
-        
+
+        public async Task<bool> AlreadySeededAsync()
+        {
+            var gameRole = await this.roleManager.FindByNameAsync(GameCreatorRoleName);
+            return gameRole != null;
+        }
+
         public async Task SeedAsync()
         {
             const string systemUsername = SystemConstants.SystemUsername;
@@ -28,16 +37,23 @@
                 throw new InvalidOperationException("A system user cannot be found and roles cannot be seeded.");
             
             // Game creator
+            await this.SeedGameCreatorAsync(systemUser);
+        }
+
+        private async Task SeedGameCreatorAsync(ApplicationUser systemUser)
+        {
             var gameCreator = new ApplicationRole
             {
-                Action = CreateGame,
-                Name = "Game creator",
+                Action = GameCreatorRoleName, 
+                Name = GameCreatorRoleName,
                 Created = DateTimeOffset.UtcNow,
-                CreatedBy = systemUser.Id,
+                CreatedBy = systemUser.Id, 
                 LastModifiedBy = systemUser.Id,
             };
 
-            await this.roleManager.CreateAsync(gameCreator);
+            var result = await this.roleManager.CreateAsync(gameCreator);
+            if (!result.Succeeded)
+                await this.logger.LogAsync($"Seeding game role did not succeed. {string.Join(',', result.Errors)}", LogSeverity.Error);
         }
     }
 }
