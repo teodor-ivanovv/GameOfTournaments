4bec57206197fba81ad11581eb928b8f5ae9e587
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-11 19:20:48 +0300

    DI registration for application user cache service.

--- src/GameOfTournaments.Web/ServiceCollectionExtensions/ApplicationServicesExtensions.cs
+++ src/GameOfTournaments.Web/ServiceCollectionExtensions/ApplicationServicesExtensions.cs
@@ -1,12 +1,14 @@
 ﻿namespace GameOfTournaments.Web.ServiceCollectionExtensions
 {
     using GameOfTournaments.Services;
+    using GameOfTournaments.Web.Cache.ApplicationUsers;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class ApplicationServicesExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
             => serviceCollection
+                .AddSingleton<IApplicationUserCache, ApplicationUserCache>()
                 .AddScoped<IJwtService, JwtService>()
                 .AddScoped<ILogger, Logger>()
                 .AddScoped<IAuditLogger, AuditLogger>()
