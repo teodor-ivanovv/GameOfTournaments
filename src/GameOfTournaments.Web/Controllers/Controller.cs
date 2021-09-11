f75c46bc1f2047fc3bb26708527b3e33139029cc
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-11 19:21:47 +0300

    Controller base enriches the current authentication context based on the cache user services.

--- src/GameOfTournaments.Web/Controllers/Controller.cs
+++ src/GameOfTournaments.Web/Controllers/Controller.cs
@@ -4,6 +4,7 @@
     using System.Security.Claims;
     using GameOfTournaments.Services;
     using GameOfTournaments.Services.Infrastructure;
+    using GameOfTournaments.Web.Cache.ApplicationUsers;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -13,24 +14,39 @@
     [Route("[controller]")]
     public abstract class Controller : ControllerBase
     {
+        private const int NegativeId = -1;
+        
+        private readonly IApplicationUserCache applicationUserCache;
+
         protected IAuthenticationService AuthenticationService { get; }
 
-        protected Controller(IHttpContextAccessor httpContextAccessor, IAuthenticationService AuthenticationService)
+        protected Controller(IHttpContextAccessor httpContextAccessor, IAuthenticationService AuthenticationService, IApplicationUserCache applicationUserCache)
         {
-            var httpContext = httpContextAccessor?.HttpContext;
+            this.applicationUserCache = applicationUserCache ?? throw new ArgumentNullException(nameof(applicationUserCache));
             this.AuthenticationService = AuthenticationService ?? throw new ArgumentNullException(nameof(AuthenticationService));
+            
+            var httpContext = httpContextAccessor?.HttpContext;
             var userId = httpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
             var ipAddress = httpContext?.Connection?.RemoteIpAddress;
             var parsed = int.TryParse(userId, out var integerId);
             if (!parsed)
-                integerId = -1;
+                integerId = NegativeId;
+
+            var cachedUser = this.applicationUserCache.Get(integerId);
+            if (cachedUser == null)
+            {
+                this.AuthenticationService.Set(new AuthenticationContext(false, integerId));
+                return;
+            }
             
             this.AuthenticationService.Set(
                 new AuthenticationContext
                 {
                     Id = integerId,
+                    Authenticated = true,
                     ApplicationUser = httpContext?.User,
                     IpAddress = ipAddress?.ToString(),
+                    Roles = cachedUser.Roles,
                 });
         }
     }
