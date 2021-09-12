7f54e518516a4f713823ad9e8f46d6d7a54c4975
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-12 19:37:16 +0300

    Tasks extensions in order to execute tasks non-blocking

---
+++ src/GameOfTournaments.Shared/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace GameOfTournaments.Shared
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static class TaskExtensions
+    {
+        public static async void ExecuteNonBlocking(this Task task)
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                // TODO: Log
+            }
+        }
+    }
+}
