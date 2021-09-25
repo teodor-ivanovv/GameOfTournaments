b40975f43baaef0708a9ffbc95d3285dceaec807
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-25 15:32:12 +0300

    Introduced a rest create game action

--- src/GameOfTournaments.Web/Controllers/GamesController.cs
+++ src/GameOfTournaments.Web/Controllers/GamesController.cs
@@ -9,6 +9,7 @@
     using GameOfTournaments.Web.Cache.ApplicationUsers;
     using GameOfTournaments.Web.Factories;
     using GameOfTournaments.Web.Models;
+    using GameOfTournaments.Web.Models.Game;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
@@ -27,7 +28,7 @@
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<GameViewModel>>> Games()
+        public async Task<ActionResult<IEnumerable<GameViewModel>>> GetGames()
         {
             if (!this.AuthenticationService.Authenticated)
                 return this.Unauthorized();
@@ -36,10 +37,25 @@
                 new GetOptions<Game, string, GameViewModel>
                 {
                     Projection = new ProjectionOptions<Game, GameViewModel>(
-                        g => GameFactory.Create(g)),
+                        g => GameFactory.CreateGameViewModel(g)),
                 });
 
             return this.Ok(games);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<CreateGameResponseModel>> CreateGame(GameViewModel gameViewModel)
+        {
+            if (!this.AuthenticationService.Authenticated)
+                return this.Unauthorized();
+
+            if (gameViewModel == null)
+                return this.BadRequest(nameof(gameViewModel));
+
+            var operationResult = await this.gameService.CreateAsync(gameViewModel.ToGame());
+            var responseModelOperationResult = operationResult.ChangeObjectType(GameFactory.CreateCreateGameResponseModel(operationResult.Object));
+
+            return this.FromOperationResult(responseModelOperationResult);
         }
     }
 }
