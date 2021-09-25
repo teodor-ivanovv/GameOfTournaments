785005997c0cfe504c281252a774dadd541478c4
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-25 15:28:16 +0300

    Introduced a new create game response model

---
+++ src/GameOfTournaments.Web/Models/Game/CreateGameResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GameOfTournaments.Web.Models.Game
+{
+    using System;
+
+    /// <summary>
+    /// Create game response model.
+    /// </summary>
+    public class CreateGameResponseModel : GameViewModel
+    {
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
