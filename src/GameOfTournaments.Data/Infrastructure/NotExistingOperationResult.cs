557d2ff1a40d69b2a81e01b247536117f5482388
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-25 16:26:28 +0300

    Created an update rest action for games. Introduced a not existing operation result type.

---
+++ src/GameOfTournaments.Data/Infrastructure/NotExistingOperationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace GameOfTournaments.Data.Infrastructure
+{
+    public class NotExistingOperationResult : NotExistingOperationResult<object>
+    {
+        public NotExistingOperationResult(string entity)
+            : base(entity)
+        {
+        }
+    }
+    
+    public class NotExistingOperationResult<T> : OperationResult<T>
+    {
+        public NotExistingOperationResult(string entity)
+        {
+            this.AddErrorMessage($"{entity} does not exist!");
+        }
+    }
+}
--- src/GameOfTournaments.Web/Controllers/GamesController.cs
+++ src/GameOfTournaments.Web/Controllers/GamesController.cs
@@ -8,7 +8,6 @@
     using GameOfTournaments.Services.Infrastructure;
     using GameOfTournaments.Web.Cache.ApplicationUsers;
     using GameOfTournaments.Web.Factories;
-    using GameOfTournaments.Web.Models;
     using GameOfTournaments.Web.Models.Game;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
@@ -28,7 +27,7 @@
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<GameViewModel>>> GetGames()
+        public async Task<ActionResult<IEnumerable<GameViewModel>>> Get()
         {
             if (!this.AuthenticationService.Authenticated)
                 return this.Unauthorized();
@@ -44,7 +43,7 @@
         }
         
         [HttpPost]
-        public async Task<ActionResult<CreateGameResponseModel>> CreateGame(GameViewModel gameViewModel)
+        public async Task<ActionResult<CreateGameResponseModel>> Create(GameViewModel gameViewModel)
         {
             if (!this.AuthenticationService.Authenticated)
                 return this.Unauthorized();
@@ -53,7 +52,22 @@
                 return this.BadRequest(nameof(gameViewModel));
 
             var operationResult = await this.gameService.CreateAsync(gameViewModel.ToGame());
-            var responseModelOperationResult = operationResult.ChangeObjectType(GameFactory.CreateCreateGameResponseModel(operationResult.Object));
+            var responseModelOperationResult = operationResult.ChangeObjectType(GameFactory.CreateGameResponseModel(operationResult.Object));
+
+            return this.FromOperationResult(responseModelOperationResult);
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult<UpdateGameResponseModel>> Update(UpdateGameModel updateGameModel)
+        {
+            if (!this.AuthenticationService.Authenticated)
+                return this.Unauthorized();
+
+            if (updateGameModel == null)
+                return this.BadRequest(nameof(updateGameModel));
+
+            var operationResult = await this.gameService.UpdateAsync(new object[] { updateGameModel.Id }, updateGameModel.ToGame());
+            var responseModelOperationResult = operationResult.ChangeObjectType(GameFactory.UpdateGameResponseModel(operationResult.Object));
 
             return this.FromOperationResult(responseModelOperationResult);
         }
