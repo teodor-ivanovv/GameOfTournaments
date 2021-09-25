27ab0dc2ea15dda49322bf1f4bf209f40b3d025d
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-25 15:32:26 +0300

    Auditing logging extensions

--- src/GameOfTournaments.Services/AuditLogger.cs
+++ src/GameOfTournaments.Services/AuditLogger.cs
@@ -1,14 +1,52 @@
 ﻿namespace GameOfTournaments.Services
 {
+    using System;
+    using System.Threading.Tasks;
+    using Ardalis.GuardClauses;
     using GameOfTournaments.Data;
     using GameOfTournaments.Data.Models;
+    using GameOfTournaments.Shared;
     using Microsoft.EntityFrameworkCore;
 
     public class AuditLogger : EfCoreService<AuditLog>, IAuditLogger
     {
-        public AuditLogger(IDbContextFactory<ApplicationDbContext> contextFactory)
-            : base(contextFactory)
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuditLogger(IDbContextFactory<ApplicationDbContext> contextFactory, IAuthenticationService authenticationService)
+            : base(contextFactory, authenticationService)
         {
+            this._authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
         }
+
+        /// <inheritdoc />
+        public Task LogAsync(AuditLog log)
+        {
+            Guard.Against.Null(log, nameof(log));
+            return this.CreateAsync(log);
+        }
+
+        public AuditLog Construct(string action, DateTimeOffset actionTime, string message, bool hasPermissions)
+        {
+            var auditLog = new AuditLog
+            {
+                Action = action,
+                ActionTime = actionTime,
+                Message = message,
+                HasPermissions = hasPermissions,
+                IpAddress = this._authenticationService.Context?.IpAddress,
+                UserId = this._authenticationService.Context?.Id,
+            };
+
+            return auditLog;
+        }
+        
+        public Action ConstructLogAction(bool inRole, string role)
+            => this.LogAsync(
+                this.Construct(
+                    role, 
+                    DateTimeOffset.UtcNow, 
+                    string.Empty,
+                    inRole))
+                .ExecuteNonBlocking;
     }
 }
--- src/GameOfTournaments.Services/IAuditLogger.cs
+++ src/GameOfTournaments.Services/IAuditLogger.cs
@@ -1,8 +1,15 @@
 ﻿namespace GameOfTournaments.Services
 {
+    using System;
+    using System.Threading.Tasks;
     using GameOfTournaments.Data.Models;
 
     public interface IAuditLogger : IEfCoreService<AuditLog>
     {
+        Task LogAsync(AuditLog log);
+
+        AuditLog Construct(string action, DateTimeOffset actionTime, string message, bool hasPermissions);
+
+        Action ConstructLogAction(bool inRole, string role);
     }
 }
