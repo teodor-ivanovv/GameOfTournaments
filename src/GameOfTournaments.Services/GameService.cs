ac90e111861cee87fdcc9881021f0a2d121a232d
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-09-25 15:32:02 +0300

    Creating games is validated with a permission for that action. Introduced a rest create game action.

--- src/GameOfTournaments.Services/GameService.cs
+++ src/GameOfTournaments.Services/GameService.cs
@@ -1,14 +1,42 @@
 ﻿namespace GameOfTournaments.Services
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using GameOfTournaments.Data;
+    using GameOfTournaments.Data.Infrastructure;
     using GameOfTournaments.Data.Models;
+    using GameOfTournaments.Shared;
     using Microsoft.EntityFrameworkCore;
 
     public class GameService : EfCoreService<Game>, IGameService
     {
-        public GameService(IDbContextFactory<ApplicationDbContext> contextFactory)
-            : base(contextFactory)
+        private readonly IAuthenticationService _authenticationService;
+
+        private readonly IAuditLogger _auditLogger;
+
+        public GameService(
+            IDbContextFactory<ApplicationDbContext> contextFactory,
+            IAuthenticationService authenticationService,
+            IAuditLogger auditLogger)
+            : base(contextFactory, authenticationService)
         {
+            this._authenticationService = authenticationService ?? throw new ArgumentNullException(nameof(authenticationService));
+            this._auditLogger = auditLogger ?? throw new ArgumentNullException(nameof(auditLogger));
+        }
+
+        public override Task<IOperationResult<Game>> CreateAsync(Game entity, CancellationToken cancellationToken = default)
+        {
+            var operationResult = new OperationResult<Game>();
+            operationResult.ValidateNotNull(entity, nameof(GameService), nameof(this.CreateAsync), nameof(entity));
+
+            var inRole = this._authenticationService.IsInRole(Roles.GameCreatorRoleName);
+            operationResult.ValidateInRole(inRole, Roles.GameCreatorRoleName, this._auditLogger.ConstructLogAction(inRole, Roles.GameCreatorRoleName));
+            
+            if (!operationResult.Success)
+                return Task.FromResult<IOperationResult<Game>>(operationResult);
+            
+            return base.CreateAsync(entity, cancellationToken);
         }
     }
 }
