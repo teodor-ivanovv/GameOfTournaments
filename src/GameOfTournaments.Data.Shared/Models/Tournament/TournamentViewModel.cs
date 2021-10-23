bc4ce8eefa850f20ac5e6c653549df59dff12ada
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-10-23 20:33:43 +0300

    Tournament view model

---
+++ src/GameOfTournaments.Data.Shared/Models/Tournament/TournamentViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GameOfTournaments.Data.Factories.Models.Tournament
+{
+    using System;
+    using GameOfTournaments.Data.Infrastructure;
+
+    public class TournamentViewModel : CreateTournamentViewModel
+    {
+        public int Id { get; set; }
+
+        public TournamentStatus Status { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+    }
+}
