eca36da0eb47eac488d2497c2516708749a41a44
Teodor Ivanov <teodor.ivanov48@gmail.com>
2021-11-06 12:54:48 +0200

    Mocking user manager

--- tests/GameOfTournamentsTests/BaseTests.cs
+++ tests/GameOfTournamentsTests/BaseTests.cs
@@ -11,8 +11,12 @@ namespace GameOfTournamentsTests
     using GameOfTournaments.Services.Infrastructure;
     using GameOfTournaments.Shared;
     using KellermanSoftware.CompareNetObjects;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using Moq;
     using Xunit;
 
     public abstract class BaseTests
@@ -32,6 +36,8 @@ namespace GameOfTournamentsTests
         #region Services
 
         protected IAuthenticationService AuthenticationService { get; private set; }
+        protected IApplicationUserService ApplicationUserService { get; private set; }
+        protected IApplicationUserAccountService ApplicationUserAccountService { get; private set; }
         protected IAuditLogger AuditLogger { get; private set; }
         protected IGameService GameService { get; private set; }
         protected ITournamentService TournamentService { get; private set; }
@@ -43,6 +49,8 @@ namespace GameOfTournamentsTests
             this.RegisterServiceProvider();
             this.AuthenticationService = this.ServiceProvider.GetRequiredService<IAuthenticationService>();
             this.AuditLogger = this.ServiceProvider.GetRequiredService<IAuditLogger>();
+            this.ApplicationUserService = this.ServiceProvider.GetRequiredService<IApplicationUserService>();
+            this.ApplicationUserAccountService = this.ServiceProvider.GetRequiredService<IApplicationUserAccountService>();
             this.GameService = this.ServiceProvider.GetRequiredService<IGameService>();
             this.TournamentService = this.ServiceProvider.GetRequiredService<ITournamentService>();
         }
@@ -144,12 +152,52 @@ namespace GameOfTournamentsTests
             this.ServiceProvider = new ServiceCollection()
                 .AddDbContextFactory<ApplicationDbContext>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()))
                 .AddScoped<IAuthenticationService, AuthenticationService>()
-                .AddScoped<IApplicationUserService, ApplicationUserService>()
+                .AddScoped<IApplicationUserService, ApplicationUserService>(s => new ApplicationUserService(MockUserManager(new List<ApplicationUser>()).Object))
                 .AddScoped<IApplicationUserAccountService, ApplicationUserAccountService>()
                 .AddScoped<IGameService, GameService>()
                 .AddScoped<ITournamentService, TournamentService>()
                 .AddScoped<IAuditLogger, AuditLogger>()
                 .BuildServiceProvider();
         }
+        
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> ls) where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+            mgr.Setup(x => x.DeleteAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+            mgr.Setup(x => x.CreateAsync(It.IsAny<TUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success).Callback<TUser, string>((x, y) => ls.Add(x));
+            mgr.Setup(x => x.UpdateAsync(It.IsAny<TUser>())).ReturnsAsync(IdentityResult.Success);
+
+            return mgr;
+        }
+        
+        // public static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser> store = null) where TUser : class
+        // {
+        //     store ??= new Mock<IUserStore<TUser>>().Object;
+        //     var options = new Mock<IOptions<IdentityOptions>>();
+        //     var idOptions = new IdentityOptions { Lockout = { AllowedForNewUsers = false } };
+        //     options.Setup(o => o.Value).Returns(idOptions);
+        //     var userValidators = new List<IUserValidator<TUser>>();
+        //     var validator = new Mock<IUserValidator<TUser>>();
+        //     userValidators.Add(validator.Object);
+        //     var pwdValidators = new List<PasswordValidator<TUser>> { new() };
+        //     var userManager = new UserManager<TUser>(
+        //         store,
+        //         options.Object,
+        //         new PasswordHasher<TUser>(),
+        //         userValidators,
+        //         pwdValidators,
+        //         new UpperInvariantLookupNormalizer(),
+        //         new IdentityErrorDescriber(),
+        //         null,
+        //         new Mock<ILogger<UserManager<TUser>>>().Object);
+        //
+        //     validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>())).Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
+        //
+        //     return userManager;
+        // }
     }
 }
